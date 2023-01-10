98f5e6a0b0918f6791356b120ef72c8f8cd57c82
Jinho Heo <uj07077@gmail.com>
2023-01-10 15:08:24 +0900

    redirection fix ongoing...

--- execute/accessibility_check.c
+++ execute/accessibility_check.c
@@ -6,7 +6,7 @@
 /*   By: jinheo <jinheo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:40:07 by jinheo            #+#    #+#             */
-/*   Updated: 2023/01/10 15:01:21 by jinheo           ###   ########.fr       */
+/*   Updated: 2023/01/10 15:08:05 by jinheo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@ static void	print_access_error(char *path)
 void	accessibility_check(char *path)
 {
 	if (path == NULL)
-		return ;
+		exit(0);
+	if (*path == '<' || *path == '>')
+		exit(0);
 	if (access(path, F_OK))
 	{
 		print_error(E_PROMPT, NULL, path, "command not found");
