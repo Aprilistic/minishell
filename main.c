5f5ae7bf10869913ccd0e3a1a0d7963ef70a02fe
Jinho Heo <uj07077@gmail.com>
2023-01-10 17:20:10 +0900

    main.c indent fixed

--- main.c
+++ main.c
@@ -6,16 +6,11 @@
 /*   By: jinheo <jinheo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:24:27 by jinheo            #+#    #+#             */
-/*   Updated: 2023/01/10 17:05:51 by jinheo           ###   ########.fr       */
+/*   Updated: 2023/01/10 17:19:49 by jinheo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	leaks(void)
-{
-	system("leaks minishell");
-}
 
 static void	retrieve_memory(t_metadata **command)
 {
@@ -39,40 +34,10 @@ static void	retrieve_memory(t_metadata **command)
 	free((*command));
 }
 
-void	check_command(t_metadata **command)
-{
-	int	command_index;
-
-	command_index = 0;
-	while ((*command)[command_index].token)
-	{
-		printf("cmd: %d, token_count: %d\n", command_index, (*command)[command_index].token_count);
-		for (int i = 0; i < (*command)[command_index].token_count; ++i){
-			printf("token %d: %s", i,(*command)[command_index].token[i]);
-			switch ((*command)[command_index].token_quote_flag[i])
-			{
-				case 1:
-					printf("(Single quoted)\n");
-				break ;
-				case 2:
-					printf("(Double quoted)\n");
-				break ;
-				default:
-					printf("\n");
-				break;
-			}
-			if ((*command)[command_index].token_quote_flag[i] == 1)
-				printf("");
-		}
-		command_index++;
-	}
-}
-
 int	main(int argc, char **argv, char **env)
 {
 	t_metadata	*command;
 
-	// atexit(&leaks);
 	(void)argv;
 	if (argc != 1)
 	{
@@ -85,15 +50,9 @@ int	main(int argc, char **argv, char **env)
 		handle_signal();
 		command = NULL;
 		if (parse_input(&command, env) == ERROR)
-		{
 			g_exit_code = 1;
-		}
 		else
 			execute(command, env);
-			
-		//parse input and execute
-
-		//retrieve_memory
 		retrieve_memory(&command);
 	}
 	return (0);
